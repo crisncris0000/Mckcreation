3149559c17efaa5b2a3f145c8e8b12d8581d26f2
crisncris0000 <christopherrivera384@gmail.com>
2024-08-25 14:23:01 -0400

    merge

--- be-app/src/main/java/com/mckcreation/be_app/service/PlacedOrdersService.java
+++ be-app/src/main/java/com/mckcreation/be_app/service/PlacedOrdersService.java
@@ -1,4 +1,14 @@
 package com.mckcreation.be_app.service;
 
+import com.mckcreation.be_app.model.PlacedOrders;
+
+import java.util.List;
+
 public interface PlacedOrdersService {
+
+    List<PlacedOrders> getPlacedOrders();
+
+    List<PlacedOrders> getUserPlacedOrders(int id);
+
+
 }
