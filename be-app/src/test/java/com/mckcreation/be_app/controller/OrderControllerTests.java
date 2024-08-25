1acb6e5f631447f3d833ea2eb7d9631d55494e1d
Christopher Rivera <christopherrivera384@gmail.com>
2024-08-25 14:16:05 -0400

    Tests

--- be-app/src/test/java/com/mckcreation/be_app/controller/OrderControllerTests.java
+++ be-app/src/test/java/com/mckcreation/be_app/controller/OrderControllerTests.java
@@ -5,7 +5,6 @@ import com.mckcreation.be_app.model.Category;
 import com.mckcreation.be_app.model.Order;
 import com.mckcreation.be_app.model.User;
 import com.mckcreation.be_app.repository.CategoryRepository;
-import com.mckcreation.be_app.repository.OrderRepository;
 import com.mckcreation.be_app.repository.UserRepository;
 import com.mckcreation.be_app.service.OrderService;
 import org.hamcrest.CoreMatchers;
@@ -25,12 +24,11 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.sql.Timestamp;
-import java.util.Base64;
-import java.util.Date;
-import java.util.Optional;
+import java.util.*;
 
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @WebMvcTest(controllers = OrderController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -115,5 +113,55 @@ public class OrderControllerTests {
                         CoreMatchers.is(orderDTO.getCustomize())))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.price",
                         CoreMatchers.is(30.0)));
+    }
+
+    @Test
+    public void OrderController_GetUserOrders_ReturnUserOrders() throws Exception {
+
+        Date date = new Date();
+        Timestamp timestamp = new Timestamp(date.getTime());
+
+        order = Order.builder()
+                .customize("Customize")
+                .price(30.0F)
+                .category(category)
+                .user(user)
+                .createdAt(timestamp)
+                .updatedAt(timestamp)
+                .build();
+
+        Order order2 = Order.builder()
+                .customize("Money")
+                .price(10F)
+                .category(category)
+                .user(user)
+                .createdAt(timestamp)
+                .updatedAt(timestamp)
+                .build();
+
+        List<Order> orders = List.of(order, order2);
+
+
+        given(orderService.getUserOrders(1)).willReturn(orders);
+
+        ResultActions response = mockMvc.perform(get("/api/order/get-orders/1")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()",
+                        CoreMatchers.is(orders.size())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[1].customize",
+                        CoreMatchers.is(order2.getCustomize())));
+
+    }
+
+    @Test
+    public void OrderController_DeleteOrder_ReturnStatusOk() throws Exception {
+        doNothing().when(orderService).deleteOrder(1, 1);
+
+        ResultActions response = mockMvc.perform(delete("/api/order/delete-order/1/1")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
